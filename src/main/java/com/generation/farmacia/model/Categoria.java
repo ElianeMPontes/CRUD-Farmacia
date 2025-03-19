abb57cb6fc0e241fed64bbf39eee07e2c189db13
ELIANEMPONTES <medeiros-eliane1@hotmail.com>
2025-03-19 10:07:55 -0300

    crud-Categoria

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,5 +1,39 @@
 package com.generation.farmacia.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity // indicação de uma nova tabela no banco de dados
+@Table(name = "tb_categoria") // indica nome da tabela a ser usada no banco de dados
 public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O Atributo Descrição é obrigatório")// nao permite passar o campo vazio
+	private String descricao;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
 
 }
